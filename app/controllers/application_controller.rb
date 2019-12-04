64bac79a32ef63d1794a9f7ee606094d1b24f5bb
James Scalise <jamescscalise@gmail.com>
2019-12-04 16:29:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,10 @@ class App < Sinatra::Base
     post "/teams" do
       @team = Team.new(params[:team])
       params[:team][:members].each do |details|
-        
+        Hero.new(details)
       end
+      
+      @heroes = Hero.all
       erb :team
     end
 
